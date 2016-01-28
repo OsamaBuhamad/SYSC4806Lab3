752ba81a396e473f888aa0c399eefc0a621dbf37
osamabuhamad <roma4ever-10@hotmail.com>
2016-01-28 15:17:21 -0500

    finished step 5

--- app/assets/javascripts/blogposts.js.coffee
+++ app/assets/javascripts/blogposts.js.coffee
@@ -1,3 +1,29 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on 'page:change', -> # initialize current word to an empty string.
+ wrd = ""
+
+ #update this line with correct selector and event
+ $("#blogpost_contents").keypress (e) -> 
+
+  # get the 1-character string that the user typed
+  inp = String.fromCharCode(e.which) 
+
+  #if this was a letter (use .match method and regular expression)
+  if (inp.match(/[a-z]/i)) 
+ #add letter to current word
+    wrd += inp
+  else
+    if (wrd.match(/\w+/i)) # if the current word is a sequence of letters
+      $.get window.location.origin + "/spellcheck/" + wrd, (data) -> 
+
+       # console.log(data)
+        if (!data.known)
+
+          #join suggestions in a single string
+
+          aString = 'The word "' + data.term + '" is not a word, did you mean: ' + data.suggestions[0] + '?'
+          $("#spellcheck").text(aString)
+     wrd = ""  #empty the word again
+
+
+
+
--- app/controllers/dictionary_words_controller.rb
+++ app/controllers/dictionary_words_controller.rb
@@ -1,24 +1,27 @@
 require "#{Rails.root}/app/models/web_spellchecker.rb"
 class DictionaryWordsController < ApplicationController
-
-  def spellcheck
-	input_word = params[:term]
-	puts "we want to check the word #{input_word}"
-	webspellchecker = WebSpellchecker.new 
-	
-
-	hash_=Hash.new
-	hash_["term"]= input_word
-	if webspellchecker.correct(input_word)
-	hash_["known"] = true	
-	else 
-	hash_["known"] = false 
-	end 
-	
-	#[optional] “suggestions”=> [“first”, “second”, “third”]
-	
-
- render json: hash_ 
-	
-  end
+ 
+ def spellcheck
+ 	webSpellchecker=WebSpellchecker.new
+ 	input_word = params[:term]
+	puts input_word
+	#render :text => "we want to check the word #{input_word}"
+	if (not input_word.nil?)
+	correct_words=webSpellchecker.correct(input_word)
+	if(not correct_words.nil?)
+		hash=Hash.new
+		knownWord=correct_words.include? input_word
+		if(knownWord)
+			hash= {"term" => input_word, "known" => knownWord}
+		else
+			hash= {"term" => input_word, "known" => knownWord, "suggestions" =>correct_words}
+		end
+	render :json =>hash
+	else
+	render :text =>"Word doesnt exist "
+  	end
+	else
+	render :text =>"No word was entered .Please enter a word!"
+	end
+	end
 end
